1dc2412228df81dd517d5d3171fe560b26308c61
Don Choi <dongha.choi@vanderbilt.edu>
2018-09-11 17:00:08 -0500

    player animations

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,38 +4,71 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
 
-    public float speed;
+    public float moveSpeed;
     public bool isSpotted;
     public bool invincible;
 
     private Rigidbody2D myRigidbody;
+    private Animator anim;
+
+    public bool playerMoving;
+    public Vector2 lastMove;
 
 	// Use this for initialization
 	void Start () {
 
         isSpotted = false;
         myRigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        playerMoving = false;
+
+        //moves player left and right
         if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1)
         {
-            myRigidbody.velocity = new Vector2(speed * Input.GetAxisRaw("Horizontal"), myRigidbody.velocity.y);
-        }
-        else
-        {
-            myRigidbody.velocity = new Vector2(0, myRigidbody.velocity.y);
+            //moves player according to moveSpeed horizontally
+            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
+
+            playerMoving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
 
-        if (Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
-        {
-            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, speed * Input.GetAxisRaw("Vertical"));
-        }
+        //otherwise don't move in x direction
         else
         {
-            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0);
+            myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
         }
+
+        //moves player up and down
+        if (Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            //moves player according to moveSpeed vertically
+            //Transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+
+            playerMoving = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+        }
+
+        //otherwise don't move in y direction
+        else
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
+        }
+
+
+
+        //assigns values to animator
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
     }
 }
