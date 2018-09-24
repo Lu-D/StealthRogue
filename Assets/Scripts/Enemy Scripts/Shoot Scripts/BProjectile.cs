638069a0eb71435103f5e153e8032aa786e3d911
Don Choi <dongha.choi@vanderbilt.edu>
2018-09-24 16:46:05 -0500

    scene update

--- Assets/Scripts/Enemy Scripts/Shoot Scripts/BProjectile.cs
+++ Assets/Scripts/Enemy Scripts/Shoot Scripts/BProjectile.cs
@@ -2,18 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD:Assets/Scripts/Enemy Scripts/Shoot Scripts/BProjectile.cs
-//Base projectile class 
-=======
 //BProjectile
 //base class for projectiles
->>>>>>> 90293a96f79c3b943260d56de07427b1dc5a209c:Assets/Scripts/BProjectile.cs
 public class BProjectile : MonoBehaviour
 {
     public float projSpeed;
     public int damage;
 
-<<<<<<< HEAD:Assets/Scripts/Enemy Scripts/Shoot Scripts/BProjectile.cs
     //initialize fields
     public virtual void Start()
     {
@@ -27,10 +22,7 @@ public class BProjectile : MonoBehaviour
     }
 
     //collision methods
-=======
-    //OnCollisionEnter2D 
     //despawns bullets which collide with out of camera border
->>>>>>> 90293a96f79c3b943260d56de07427b1dc5a209c:Assets/Scripts/BProjectile.cs
     public virtual void OnCollisionEnter2D(Collision2D collision)
     {
         //Destroy projectile when colliding with game border
