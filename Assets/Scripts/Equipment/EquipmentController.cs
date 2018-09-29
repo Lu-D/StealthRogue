0de29a3d5bee381c940e3ffa3a6f2bc3d4bcb5e2
Daiwei Lu <daiwei.lu@vanderbilt.edu>
2018-09-29 15:29:51 -0500

    equipment abstract class

---
+++ Assets/Scripts/Equipment/EquipmentController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EquipmentController : MonoBehaviour {
+    //variables Equipments modify
+    public float playerSpeedBonus = 0;
+
+    public abstract void onKeyDown();
+	
+}
