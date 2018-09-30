346be9bd9ef9581cdba97f204cdf89316e43c348
Don Choi <dongha.choi@vanderbilt.edu>
2018-09-30 16:04:34 -0500

    scene update

--- Assets/Scripts/Map/LocatePlayer.cs
+++ Assets/Scripts/Map/LocatePlayer.cs
@@ -4,13 +4,5 @@ using UnityEngine;
 
 public class LocatePlayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Physics2D
 }
