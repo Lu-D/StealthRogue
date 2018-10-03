a0409441428a33a09cd0d59e6009504d3b463f5d
Don Choi <dongha.choi@vanderbilt.edu>
2018-10-03 10:48:18 -0500

    gate prefab update

---
+++ Assets/Scripts/Map/GateCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateCollision : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Physics2D.IgnoreLayerCollision(13, 14, true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Physics2D.IgnoreLayerCollision(13, 14, false);
+        }
+    }
+}
