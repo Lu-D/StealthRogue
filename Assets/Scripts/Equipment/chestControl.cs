034e83d143e9dc176ba3afb518d5dcc5b2c3f2e7
Daiwei Lu <daiwei.lu@vanderbilt.edu>
2018-10-11 18:17:38 -0500

    random equipment spawn from chest

---
+++ Assets/Scripts/Equipment/chestControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chestControl : MonoBehaviour {
+    public GameObject[] drops;
+	
+    void Awake()
+    {
+
+    }
+
+    public void OnCollisionEnter2D (Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            dropItem();
+        }
+    }
+
+    void dropItem()
+    {
+        System.Random rnd = new System.Random();
+        spawnItem(drops[UnityEngine.Random.Range(0,2)]);
+    }
+
+    void spawnItem(GameObject type)
+    {
+        GameObject dropped = (GameObject)Instantiate(type, transform.position, transform.rotation);
+        dropped.transform.position = dropped.transform.position + new Vector3(0, (float)-0.2, 0);
+        dropped.GetComponent<Collider2D>().enabled = true;
+        Destroy(this.gameObject);
+    }
+
+
+
+
+}
