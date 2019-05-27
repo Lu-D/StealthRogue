1023ecb9942fe1d1d575fd7846ed0024258d0c98
Dongha Choi <dongha.choi@vanderbilt.edu>
2019-05-27 15:30:58 -0500

    boss health bar added

---
+++ Assets/Scripts/UI/BossLife.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossLife : MonoBehaviour
+{
+    private Slider slider;
+    private BEnemy boss;
+    private PlayerControl player;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+
+        boss = GameObject.Find("Eater Boss").GetComponent<BEnemy>();
+        player = GameObject.Find("Player").GetComponent<PlayerControl>();
+    }
+
+    private void Start()
+    {
+        slider.maxValue = boss.health;
+    }
+
+    private void Update()
+    {
+        slider.value = boss.health;
+    }
+}
+
